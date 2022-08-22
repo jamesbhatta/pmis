8a0115f30a4b98da5ff4e8cf0fa143bc3b21a64d
sgrkathariya <sgrkathariya@gmail.com>
2022-08-22 14:57:43 +0545

    wrp

--- database/seeders/FiscalYearSeeder.php
+++ database/seeders/FiscalYearSeeder.php
@@ -15,11 +15,14 @@ class FiscalYearSeeder extends Seeder
     public function run()
     {
         // Eloquent::unguard();
-        // $path = database_path('seeders/files/fiscal_years.sql');
-        // DB::unprepared(file_get_contents($path));
-        // $this->command->info('Fiscal year table seeded!');
+        $path = database_path('seeders/files/fiscal_years.sql');
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Fiscal year table seeded!');
 
         // DB::table('fiscal_years')->insert([
+        //     'start_ad'=>'2020-01-01',
+        //     'end_ad'=>'2020-12-31',
+        //     'name' => '2019-2020',
         //     'start' => "2078-01-01",
         //     'end' => "2078-12-31",
         // ]);
