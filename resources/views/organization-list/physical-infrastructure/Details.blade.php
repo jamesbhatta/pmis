40bcdaee54a44eb9445c729e11253da6b0f90191
sgrkathariya <sgrkathariya@gmail.com>
2022-09-11 15:56:38 +0545

    wrp

--- resources/views/organization-list/physical-infrastructure/Details.blade.php
+++ resources/views/organization-list/physical-infrastructure/Details.blade.php
@@ -12,7 +12,6 @@
     var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
     var yValues = [55, 49, 44, 24, 15];
     var barColors = ["red", "green", "blue", "orange", "brown"];
-
     new Chart("myChart", {
         type: "bar",
         data: {
