ac2647b801d54708cbed497df392901487d0dd1a
sgrkathariya <sgrkathariya@gmail.com>
2022-09-20 17:37:56 +0545

    wrp

--- resources/views/organization-list/physical-infrastructure/allprojects.blade.php
+++ resources/views/organization-list/physical-infrastructure/allprojects.blade.php
@@ -5,7 +5,7 @@
     <div style="height: 150px;"></div>
     <div class="container">
         <h2 class="mt-2">
-        जिल्ला कैलाली अंतर्गत परियोजनाहरु
+            जिल्ला कैलाली अंतर्गत परियोजनाहरु
         </h2>
         <div class="row d-flex flex-row">
             <div class="row col-6">
@@ -14,13 +14,14 @@
                         <th scope="col">#</th>
                         <th scope="col">जिल्लागत परियोजना</th>
                         <th scope="col">कुल परियोजना</th>
-                        <th scope="col">Status</th>
+                        <th scope="col">स्थिति</th>
                     </thead>
                     <tbody>
                         <tr class="fontsize1">
                             <td><a href="{{route('all-projects')}}">1</a></td>
                             <td><a href="{{route('all-projects')}}">Kailali</a></td>
                             <td><a href="{{route('all-projects')}}">10</a></td>
+                            <td><a href="{{route('all-projects')}}">सम्पन्न</a></td>
                         </tr>
                         <tr class="fontsize1">
                             <td><a href="{{route('view-details')}}">2</a></td>
@@ -58,8 +59,8 @@
                             <td><a href="{{route('view-details')}}">12</a></td>
                         </tr>
                         <tr class="fontsize1">
-                            <td><a href="{{route('view-details')}}">3</a></td>
-                            <td><a href="{{route('view-details')}}">Doti</a></td>
+                            <td><a href="{{route('view-details')}}" class="filter on_going">3</a></td>
+                            <td><a href="{{route('view-details')}}" cl>Doti</a></td>
                             <td><a href="{{route('view-details')}}">20</a></td>
                         </tr>
                         <tr class="fontsize1">
@@ -81,9 +82,9 @@
                 </table>
             </div>
             <div id="myBtnContainer" style="height: 20px;" class="row col-5 ml-5 form-select" aria-label="Default select example">
-                <button class="btn active" onclick="filterSelection('all')">All Projects</button>
-                <button class="btn" onclick="filterSelection('ongoing')">On Going</button>
-                <button class="btn" onclick="filterSelection('completed')">Completed</button>
+                <button class="btn active filterDiv" onclick="filterSelection('all')">All Projects</button>
+                <button class="btn filterDiv" onclick="filterSelection('on_going')">On Going</button>
+                <button class="btn filterDiv" onclick="filterSelection('completed')">Completed</button>
             </div>
         </div>
     </div>
@@ -92,50 +93,47 @@
 @endsection
 <script>
     filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
 
-    function filterSelection(c) {
-        var x, i;
-        x = document.getElementsByClassName("filterDiv");
-        if (c == "all") c = "";
-        for (i = 0; i < x.length; i++) {
-            RemoveClass(x[i], "show");
-            if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-        }
-    }
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
 
-    function AddClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-            if (arr1.indexOf(arr2[i]) == -1) {
-                element.className += " " + arr2[i];
-            }
-        }
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
+  }
+  element.className = arr1.join(" ");
+}
 
-    function RemoveClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-            while (arr1.indexOf(arr2[i]) > -1) {
-                arr1.splice(arr1.indexOf(arr2[i]), 1);
-            }
-        }
-        element.className = arr1.join(" ");
-    }
-
-    // Add active class to the current button (highlight it)
-    var btnContainer = document.getElementById("myBtnContainer");
-    var btns = btnContainer.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function() {
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
-    }
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 </script>
 <style>
     #myBtnContainer {
@@ -152,6 +150,7 @@
         background-color: #f1f1f1;
         cursor: pointer;
     }
+
     .btn:hover {
         background-color: #ddd;
     }
